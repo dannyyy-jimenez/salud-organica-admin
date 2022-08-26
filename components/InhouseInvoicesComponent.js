7e8f746c241a98762cab9334ba3eed5eeafc97a9
Daniel Jimenez <daniel.jimenez@aurodim.com>
2022-08-25 22:32:27 -0500

    barcode permission

--- components/InhouseInvoicesComponent.js
+++ components/InhouseInvoicesComponent.js
@@ -271,7 +271,7 @@ export default function InvoicesComponent({navigation, route}) {
 
   React.useEffect(() => {
     (async () => {
-      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      await BarCodeScanner.requestPermissionsAsync();
       let { status } = await Location.requestForegroundPermissionsAsync();
 
       if (status !== 'granted') {
